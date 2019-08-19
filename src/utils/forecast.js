f1fccb16875782cb62d82ba9c774c99c7c38ad8b
Alex Rouault <rouaultalex@gmail.com>
2019-08-19 15:55:35 -0600

    Added daily high and low temperatures

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,8 +10,9 @@ const forecast = (latitude, longitude, callback) => {
                 callback("Unable to find location", undefined)
             } else {
                 callback(undefined, body.daily.data[0].summary +
-                    'It is currently ' + body.currently.temperature + ' degrees out.' + 
-                    'There is a ' + body.currently.precipProbability * 100 + '% chance of rain.'
+                    'It is currently ' + body.currently.temperature + ' degrees out. ' + 
+                    'There is a ' + body.currently.precipProbability * 100 + '% chance of rain. ' +
+                    'The daily high is ' + body.daily.data[0].temperatureHigh + ' degrees, and daily low is ' + body.daily.data[0].temperatureLow + ' degrees. '
                 )
             }
         })
